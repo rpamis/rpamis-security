ed31e688eb4557d99d722e03c75d95c326a8b311
benym <benyuanming@gmail.com>
2026-04-08 18:24:23 +0800

    fix: compatibility with multiple plugin proxies

--- rpamis-security-core/src/main/java/com/rpamis/security/core/mybatis/MybatisEncryptInterceptor.java
+++ rpamis-security-core/src/main/java/com/rpamis/security/core/mybatis/MybatisEncryptInterceptor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023-2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.rpamis.security.core.mybatis;
 
 import com.rpamis.security.core.mybatisplus.Constants;
@@ -21,111 +36,109 @@ import java.util.Map;
 import java.util.Objects;
 
 /**
- * Mybatis加密拦截器，入库时加密，查询时加密做等值查询
- * 同时兼容mybatis plus
- * 区别于
- * @see MybatisDynamicSqlEncryptInterceptor
- * 这个插件的在其后执行，在正确修改动态sql加密后，对对象进行深度克隆
- * 避免save或update之后还需要对对象进行操作，但对象已经被加密的问题
- * 同时处理非foreach等动态sql的克隆加密
+ * Mybatis加密拦截器，入库时加密，查询时加密做等值查询 同时兼容mybatis plus 区别于
  *
+ * @see MybatisDynamicSqlEncryptInterceptor 这个插件的在其后执行，在正确修改动态sql加密后，对对象进行深度克隆
+ * 避免save或update之后还需要对对象进行操作，但对象已经被加密的问题 同时处理非foreach等动态sql的克隆加密
  * @author benym
  * @since 2023/8/31 16:17
  */
-@Intercepts({
-        @Signature(type = ParameterHandler.class, method = "setParameters",
-                args = PreparedStatement.class)
-})
+@Intercepts({ @Signature(type = ParameterHandler.class, method = "setParameters", args = PreparedStatement.class) })
 public class MybatisEncryptInterceptor implements Interceptor {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MybatisEncryptInterceptor.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(MybatisEncryptInterceptor.class);
 
-    private final SecurityResolver securityResolver;
+	private final SecurityResolver securityResolver;
 
-    public MybatisEncryptInterceptor(SecurityResolver securityResolver) {
-        this.securityResolver = securityResolver;
-    }
+	public MybatisEncryptInterceptor(SecurityResolver securityResolver) {
+		this.securityResolver = securityResolver;
+	}
 
-    @Override
-    @SuppressWarnings("all")
-    public Object intercept(Invocation invocation) throws Throwable {
-        try {
-            if (!(invocation.getTarget() instanceof ParameterHandler)) {
-                return invocation.proceed();
-            }
-            ParameterHandler statementHandler = PluginUtils.realTarget(invocation.getTarget());
-            MetaObject metaObject = SystemMetaObject.forObject(statementHandler);
-            // 如果是select操作，或者mybatis-plus的@SqlParser(filter = true)跳过该方法解析，不进行验证
-            MappedStatement mappedStatement = (MappedStatement) metaObject.getValue("mappedStatement");
-            if (SqlCommandType.SELECT.equals(mappedStatement.getSqlCommandType())) {
-                return invocation.proceed();
-            }
-            ParameterHandler parameterHandler = (ParameterHandler) invocation.getTarget();
-            Object parameterObject = parameterHandler.getParameterObject();
-            if (Objects.isNull(parameterObject)) {
-                return invocation.proceed();
-            }
-            // mybatis对于List类型的处理，底层默认为Map类型
-            if (parameterObject instanceof Map) {
-                Map<String, Object> parameterObjectMap = (Map<String, Object>) parameterObject;
-                // 如果不包含mybatis-plus的实体key
-                if (parameterObjectMap.containsKey(Constants.ENTITY)) {
-                    Object entity = parameterObjectMap.get(Constants.ENTITY);
-                    if (entity != null) {
-                        // 深拷贝复制
-                        Object deepCloneEntity = SerializationUtils.deepClone(entity);
-                        if (Objects.nonNull(deepCloneEntity)) {
-                            // 进行加密
-                            Object encryptObject = securityResolver.encryptFiled(deepCloneEntity);
-                            parameterObjectMap.put(Constants.ENTITY, encryptObject);
-                        }
-                    }
-                }
-            } else {
-                // mybatis处理
-                Object deepCloneEntity = SerializationUtils.deepClone(parameterObject);
-                if (Objects.nonNull(deepCloneEntity)) {
-                    // 兼容 MyBatis Plus 3.5.7+（移除了子类 parameterObject 字段）
-                    // 3.5.6-：子类有自己的 parameterObject，getParameterObject() 返回子类字段，必须设置子类
-                    // 3.5.7+：子类无此字段，getParameterObject() 返回父类字段，必须设置父类
-                    boolean setSuccess = false;
-                    try {
-                        // 优先尝试子类的 parameterObject（兼容 3.5.6 及以下）
-                        Field field = parameterHandler.getClass().getDeclaredField("parameterObject");
-                        field.setAccessible(true);
-                        field.set(parameterHandler, deepCloneEntity);
-                        setSuccess = true;
-                    }
-                    catch (NoSuchFieldException | SecurityException e) {
-                        // 子类无此字段，尝试父类（兼容 3.5.7+）
-                        try {
-                            Class<?> superclass = parameterHandler.getClass().getSuperclass();
-                            if (DefaultParameterHandler.class.equals(superclass)) {
-                                Field superField = superclass.getDeclaredField("parameterObject");
-                                superField.setAccessible(true);
-                                superField.set(parameterHandler, deepCloneEntity);
-                                setSuccess = true;
-                            }
-                        }
-                        catch (NoSuchFieldException | SecurityException ex) {
-                            LOGGER.warn("Failed to set parameterObject, encryption may be invalid", ex);
-                        }
-                    }
-                    if (setSuccess) {
-                        // 进行加密
-                        Object encryptObject = securityResolver.encryptFiled(deepCloneEntity);
-                    }
-                }
-            }
-            return invocation.proceed();
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            // 如果是invocation.proceed()过程执行真实方法异常了，则直接抛出，非security插件问题
-            throw e;
-        } catch (Exception e) {
-            LOGGER.error("encrypt process: rpamis security mybatis encrypt interceptor unknown exception, the original value is returned, and no impact is caused", e);
-            return invocation.proceed();
-        }
-    }
+	@Override
+	@SuppressWarnings("all")
+	public Object intercept(Invocation invocation) throws Throwable {
+		try {
+			if (!(invocation.getTarget() instanceof ParameterHandler)) {
+				return invocation.proceed();
+			}
+			ParameterHandler statementHandler = PluginUtils.realTarget(invocation.getTarget());
+			MetaObject metaObject = SystemMetaObject.forObject(statementHandler);
+			// 如果是select操作，或者mybatis-plus的@SqlParser(filter = true)跳过该方法解析，不进行验证
+			MappedStatement mappedStatement = (MappedStatement) metaObject.getValue("mappedStatement");
+			if (SqlCommandType.SELECT.equals(mappedStatement.getSqlCommandType())) {
+				return invocation.proceed();
+			}
+			ParameterHandler parameterHandler = PluginUtils.realTarget(invocation.getTarget());
+			Object parameterObject = parameterHandler.getParameterObject();
+			if (Objects.isNull(parameterObject)) {
+				return invocation.proceed();
+			}
+			// mybatis对于List类型的处理，底层默认为Map类型
+			if (parameterObject instanceof Map) {
+				Map<String, Object> parameterObjectMap = (Map<String, Object>) parameterObject;
+				// 如果不包含mybatis-plus的实体key
+				if (parameterObjectMap.containsKey(Constants.ENTITY)) {
+					Object entity = parameterObjectMap.get(Constants.ENTITY);
+					if (entity != null) {
+						// 深拷贝复制
+						Object deepCloneEntity = SerializationUtils.deepClone(entity);
+						if (Objects.nonNull(deepCloneEntity)) {
+							// 进行加密
+							Object encryptObject = securityResolver.encryptFiled(deepCloneEntity);
+							parameterObjectMap.put(Constants.ENTITY, encryptObject);
+						}
+					}
+				}
+			}
+			else {
+				// mybatis处理
+				Object deepCloneEntity = SerializationUtils.deepClone(parameterObject);
+				if (Objects.nonNull(deepCloneEntity)) {
+					// 兼容 MyBatis Plus 3.5.7+（移除了子类 parameterObject 字段）
+					// 3.5.6-：子类有自己的 parameterObject，getParameterObject() 返回子类字段，必须设置子类
+					// 3.5.7+：子类无此字段，getParameterObject() 返回父类字段，必须设置父类
+					boolean setSuccess = false;
+					try {
+						// 优先尝试子类的 parameterObject（兼容 3.5.6 及以下）
+						Field field = parameterHandler.getClass().getDeclaredField("parameterObject");
+						field.setAccessible(true);
+						field.set(parameterHandler, deepCloneEntity);
+						setSuccess = true;
+					}
+					catch (NoSuchFieldException | SecurityException e) {
+						// 子类无此字段，尝试父类（兼容 3.5.7+）
+						try {
+							Class<?> superclass = parameterHandler.getClass().getSuperclass();
+							if (DefaultParameterHandler.class.equals(superclass)) {
+								Field superField = superclass.getDeclaredField("parameterObject");
+								superField.setAccessible(true);
+								superField.set(parameterHandler, deepCloneEntity);
+								setSuccess = true;
+							}
+						}
+						catch (NoSuchFieldException | SecurityException ex) {
+							LOGGER.warn("Failed to set parameterObject, encryption may be invalid", ex);
+						}
+					}
+					if (setSuccess) {
+						// 进行加密
+						Object encryptObject = securityResolver.encryptFiled(deepCloneEntity);
+					}
+				}
+			}
+			return invocation.proceed();
+		}
+		catch (IllegalAccessException | InvocationTargetException e) {
+			// 如果是invocation.proceed()过程执行真实方法异常了，则直接抛出，非security插件问题
+			throw e;
+		}
+		catch (Exception e) {
+			LOGGER.error(
+					"encrypt process: rpamis security mybatis encrypt interceptor unknown exception, the original value is returned, and no impact is caused",
+					e);
+			return invocation.proceed();
+		}
+	}
 
     @Override
     public Object plugin(Object target) {
